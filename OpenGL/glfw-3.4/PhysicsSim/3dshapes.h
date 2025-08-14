ed7b1cc9e80464e2bc9fab35135a8f53cb9850e3
Invisipac <invisiblepacman@gmail.com>
2025-08-14 13:11:17 -0400

    Add cube class and 3D shape header

---
+++ OpenGL/glfw-3.4/PhysicsSim/3dshapes.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include<glad/glad.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+
+class ThreeDShape {
+	std::vector<glm::vec3> vertices;
+	glm::vec3 position;
+	glm::vec3 velocity;
+	glm::vec3 acceleration;
+	float mass;
+
+	void SetVertices();
+
+	void UpdateVertexBuffer();
+
+	void DrawShape();
+
+};
---
+++ OpenGL/glfw-3.4/PhysicsSim/cube.cpp
@@ -0,0 +1,10 @@
+#include "3dshapes.h"
+#include<glad/glad.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+
+
+class Cube : ThreeDShape {
+
+};
