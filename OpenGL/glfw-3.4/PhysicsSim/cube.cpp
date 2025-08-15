55d2dc817896296d3c6d8d51d675b9c7f6a46649
invisipac <invisiblepacman@gmail.com>
2025-08-14 23:39:40 -0400

    Start adding to cube class

--- OpenGL/glfw-3.4/PhysicsSim/cube.cpp
+++ OpenGL/glfw-3.4/PhysicsSim/cube.cpp
@@ -6,5 +6,17 @@
 
 
 class Cube : ThreeDShape {
+	float s;
+
+	void SetCubeVertices() {
+		glm::vec3 ftl = glm::vec3({ -s, s, s });
+		glm::vec3 ftr = glm::vec3({ s, s, s });
+		glm::vec3 fbl = glm::vec3({ -s, s, -s });
+		glm::vec3 fbr = glm::vec3({ s, s, -s });
+		glm::vec3 btl = glm::vec3({ -s, -s, s });
+		glm::vec3 ftr = glm::vec3({ s, -s, s });
+		glm::vec3 fbl = glm::vec3({ -s, -s, -s });
+		glm::vec3 fbr = glm::vec3({ s, -s, -s });
+	}
 
 };
